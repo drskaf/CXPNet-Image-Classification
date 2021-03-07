787b1666865c20369be602fec922a345732723a1
drskaf <73792019+drskaf@users.noreply.github.com>
2021-03-07 09:38:10 +0000

    Create evaluate_model.py

---
+++ evaluate_model.py
@@ -0,0 +1,41 @@
+# Import packages
+from sklearn.preprocessing import binarize
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score, plot_precision_recall_curve, f1_score, confusion_matrix
+
+def plot_auc(t_y, p_y):
+    
+    fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+    fpr, tpr, thresholds = roc_curve(t_y, p_y)
+    c_ax.plot(fpr, tpr, label = '%s (AUC:%0.2f)'  % ('Pneumonia', auc(fpr, tpr)))
+    c_ax.legend()
+    c_ax.set_xlabel('False Positive Rate')
+    c_ax.set_ylabel('True Positive Rate')
+    
+def plot_precision_recall_curve(t_y, p_y):
+    
+
+    fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
+    precision, recall, thresholds = precision_recall_curve(t_y, p_y)
+    c_ax.plot(recall, precision, label = '%s (AP Score:%0.2f)'  % ('Pneumonia', average_precision_score(t_y,p_y)))
+    c_ax.legend()
+    c_ax.set_xlabel('Recall')
+    c_ax.set_ylabel('Precision')
+    
+def calc_f1(prec,recall):
+    return 2*(prec*recall)/(prec+recall)
+  
+def plot_history(history):
+    
+    
+    N = len(history.history["loss"])
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(np.arange(0, N), history.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, N), history.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, N), history.history["binary_accuracy"], label="train_acc")
+    plt.plot(np.arange(0, N), history.history["val_binary_accuracy"], label="val_acc")
+    plt.title("Training Loss and Accuracy on Dataset")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    
